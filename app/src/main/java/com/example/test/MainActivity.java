40d7e2634cc9b784d7207767c563e0b5ae868fda
minerva0394 <58332372+minerva0394@users.noreply.github.com>
2021-10-14 21:13:52 +0800

    2021.10.14 android studio 第一次实验报告 提交

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +12,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_TEST);
+        //todo java代码中，获取图片资源失效，需要解决
         setContentView(R.layout.activity_main);
+        getResources().getDimension(R.dimen.activity_horizontal_margin);
         Log.v("MainActivity","Verbose");
         Log.d("MainActivity", "Debug");
         Log.i("MainActivity", "Info");
