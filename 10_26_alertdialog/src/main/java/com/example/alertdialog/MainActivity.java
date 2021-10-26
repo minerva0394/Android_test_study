992d031cdaa5374bbb9cbbf610d14ed44347f0fa
minerva0394 <58332372+minerva0394@users.noreply.github.com>
2021-10-26 11:28:39 +0800

    2021.10.26 RadioGroup&AlertDialog

---
+++ 10_26_alertdialog/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog dialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("普通")
+                .setIcon(R.mipmap.ic_launcher)
+                .setMessage("是否退出应用")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        dialog = builder.create();
+        dialog.show();
+    }
+}
